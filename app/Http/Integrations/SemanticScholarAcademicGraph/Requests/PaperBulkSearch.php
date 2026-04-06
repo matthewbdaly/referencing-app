94ca9660fb63b4c61459c50795dcfc107bfeb85d
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2026-04-06 11:51:28 +0100

    Updated standard

--- app/Http/Integrations/SemanticScholarAcademicGraph/Requests/PaperBulkSearch.php
+++ app/Http/Integrations/SemanticScholarAcademicGraph/Requests/PaperBulkSearch.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Integrations\SemanticScholarAcademicGraph\Requests;
 
 use Illuminate\Support\Facades\Cache;
--- tests/Feature/Models/TaxonomyTest.php
+++ tests/Feature/Models/TaxonomyTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use App\Models\Taxonomy;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
