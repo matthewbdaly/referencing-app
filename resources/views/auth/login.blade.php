3460e85e66ab74020fb58c0799fa76d09a651b89
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2026-04-06 15:16:31 +0100

    Fixed a discrepancy in field names that broke login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,11 +3,11 @@
         <main class="">
             <form method="POST" action="{{ route('login.store') }}" class="grid grid-cols-[100px_1fr] items-center gap-4 max-w-md">
                 @csrf
-                @error('username')
+                @error('email')
                 <div class="w-full p-2 bg-pink-600 text-white col-span-2">{{ $message }}</div>
                 @enderror
-                <label for="username" class="text-sm font-medium text-gray-700">Email address</label>
-                <input id="username" name="username" type="email" required autocomplete="email" placeholder="Email address" class="rounded-md border border-gray-300 p-2 invalid:border-pink-600 invalid:text-pink-600 focus:ring-2 focus:ring-blue-500 focus:invalid:ring-pink-600 outline-none" value="{{ old('username') }}" />
+                <label for="email" class="text-sm font-medium text-gray-700">Email address</label>
+                <input id="email" name="email" type="email" required autocomplete="email" placeholder="Email address" class="rounded-md border border-gray-300 p-2 invalid:border-pink-600 invalid:text-pink-600 focus:ring-2 focus:ring-blue-500 focus:invalid:ring-pink-600 outline-none" value="{{ old('email') }}" />
                 @error('password')
                 <div class="w-full p-2 bg-pink-600 text-white col-span-2">{{ $message }}</div>
                 @enderror
