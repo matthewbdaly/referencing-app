18232c1bba64edea150e91e6f949f5def8cfeac9
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2026-04-06 11:24:05 +0100

    Can now get paper details

--- app/Http/Integrations/SemanticScholarAcademicGraph/Requests/PaperDetails.php
+++ app/Http/Integrations/SemanticScholarAcademicGraph/Requests/PaperDetails.php
@@ -5,29 +5,28 @@ namespace App\Http\Integrations\SemanticScholarAcademicGraph\Requests;
 use Saloon\Enums\Method;
 use Saloon\Http\Request;
 
-final class PaperBulkSearch extends Request
+class PaperDetails extends Request
 {
     /**
      * The HTTP method of the request
      */
     protected Method $method = Method::GET;
 
-    public function __construct(public readonly string $terms) {}
+    public function __construct(public readonly string $paperId) {}
 
     /**
      * The endpoint for the request
      */
     public function resolveEndpoint(): string
     {
-        return 'graph/v1/paper/search/bulk';
+        return 'graph/v1/paper/' . $this->paperId;
     }
 
     protected function defaultQuery(): array
     {
         return [
-            'query' => urlencode($this->terms),
-            'fields' => 'title,url,publicationTypes,publicationDate,openAccessPdf',
-            'year' => '2023-',
+            'fields' => 'title,year,abstract,citationCount',
         ];
     }
+
 }
